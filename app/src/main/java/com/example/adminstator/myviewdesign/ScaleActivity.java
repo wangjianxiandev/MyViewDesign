6404a06872635c060b937824dc56fd5af44a89fe
wangjianxiandev <jianxianwang198@163.com>
2019-05-22 15:42:20 +0800

    插值器

--- app/src/main/java/com/example/adminstator/myviewdesign/ScaleActivity.java
+++ app/src/main/java/com/example/adminstator/myviewdesign/ScaleActivity.java
@@ -4,9 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.OvershootInterpolator;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
@@ -29,26 +39,26 @@ public class ScaleActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ScaleAnimation scaleAnimation = new ScaleAnimation(0.0f, 1.4f, 0.0f, 1.4f, Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                scaleAnimation.setRepeatCount(2);
-                scaleAnimation.setDuration(700);
-                textView.startAnimation(scaleAnimation);
-                scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-                        Log.d("ScaleActivity", "onAnimationStart");
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        Log.d("ScaleActivity", "onAnimationEnd");
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-                        Log.d("ScaleActivity", "onAnimationRepeat");
-                    }
-                });
+//                ScaleAnimation scaleAnimation = new ScaleAnimation(0.0f, 1.4f, 0.0f, 1.4f, Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+//                scaleAnimation.setRepeatCount(2);
+//                scaleAnimation.setDuration(700);
+//                textView.startAnimation(scaleAnimation);
+//                scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+//                    @Override
+//                    public void onAnimationStart(Animation animation) {
+//                        Log.d("ScaleActivity", "onAnimationStart");
+//                    }
+//
+//                    @Override
+//                    public void onAnimationEnd(Animation animation) {
+//                        Log.d("ScaleActivity", "onAnimationEnd");
+//                    }
+//
+//                    @Override
+//                    public void onAnimationRepeat(Animation animation) {
+//                        Log.d("ScaleActivity", "onAnimationRepeat");
+//                    }
+//                });
 
 
 //                AlphaAnimation alphaAnimation = new AlphaAnimation(0.0f, 1.0f);
@@ -77,6 +87,42 @@ public class ScaleActivity extends AppCompatActivity {
 //                animationSet.setDuration(3000);
 //                animationSet.setFillAfter(true);
 //                textView.startAnimation(animationSet);
+
+                //插值器
+                TranslateAnimation translateAnimationInter = (TranslateAnimation) AnimationUtils.loadAnimation(ScaleActivity.this, R.anim.translateinter);
+
+//                先快后慢
+//                translateAnimationInter.setInterpolator(new AccelerateDecelerateInterpolator());
+
+//                加速突然停止
+//                translateAnimationInter.setInterpolator(new AccelerateInterpolator());
+
+//                减速插值器
+//                translateAnimationInter.setInterpolator(new DecelerateInterpolator());
+
+//                匀速插值器
+//                translateAnimationInter.setInterpolator(new LinearInterpolator());
+
+
+//                弹跳插值器
+//                translateAnimationInter.setInterpolator(new BounceInterpolator());
+
+//                反向初始插值器
+//                translateAnimationInter.setInterpolator(new AnticipateInterpolator(4));
+
+//                前向结束插值器
+//                translateAnimationInter.setInterpolator(new OvershootInterpolator());
+
+//                上述两者结合
+//                translateAnimationInter.setInterpolator(new AnticipateOvershootInterpolator(2,2));
+
+//                循环插值器
+//                translateAnimationInter.setInterpolator(new CycleInterpolator(1));
+
+
+
+                textView.startAnimation(translateAnimationInter);
+
 
 
             }
