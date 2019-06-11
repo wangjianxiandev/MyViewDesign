4d63f99f57989ed36ba51eb0106520abe56c21fa
wangjianxiandev <jianxianwang198@163.com>
2019-06-11 20:27:07 +0800

    svg-path

---
+++ app/src/main/java/com/example/adminstator/myviewdesign/DongHuaJinJie/SVGExample/SVG_First_Activity.java
@@ -0,0 +1,15 @@
+package com.example.adminstator.myviewdesign.DongHuaJinJie.SVGExample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.adminstator.myviewdesign.R;
+
+public class SVG_First_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_svg__first_);
+    }
+}
